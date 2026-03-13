e35b3ad50071e2520d687acf6f08614688b9d6fe
Zenith <waedatwi10@gmail.com>
2026-03-13 13:38:43 +0200

    Refactor

--- frontend/frontend.client/src/App.jsx
+++ frontend/frontend.client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import "leaflet/dist/leaflet.css";
 
-//loginpages
+// login pages
 import LoginPage from "./pages/LoginPage/LoginPage";
 import SignupPage from "./pages/LoginPage/SignupPage";
 import ForgotPasswordPage from "./pages/LoginPage/ForgotPasswordPage";
@@ -9,7 +9,7 @@ import ResetPasswordPage from "./pages/LoginPage/ResetPasswordPage";
 import OAuthCallbackPage from "./pages/LoginPage/OAuthCallbackPage";
 import EmailConfirmed from "./pages/LoginPage/EmailConfirmed";
 
-//job details pages
+// job details pages
 import ProfileReviewPage from "./pages/JobDetails/ProfileReviewPage";
 import ApplicationReviewPage from "./pages/JobDetails/ApplicationReviewPage";
 import AssessmentPage from "./pages/JobDetails/AssesmentsPage";
@@ -29,7 +29,6 @@ import ProfilePage from "./pages/ProfilePage";
 import Dashboard from "./pages/Dashboard";
 
 export default function App() {
-    console.log("API_BASE =", import.meta.env.VITE_API_BASE_URL);
     return (
         <Routes>
             <Route path="/" element={<Navigate to="/login" replace />} />
@@ -41,26 +40,18 @@ export default function App() {
             <Route path="/email-confirmed" element={<EmailConfirmed />} />
 
             <Route element={<AppLayout />}>
-                <Route path="/dashboard" element={<DashboardRoute />} />
+                <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/browse" element={<BrowseOpportunities />} />
                 <Route path="/match" element={<Match />} />
                 <Route path="/organization" element={<OrganizationDashboard />} />
                 <Route path="/opportunities/:id" element={<JobDetailsPage />} />
                 <Route path="/apply/:applicationId/review" element={<ProfileReviewPage />} />
-
                 <Route path="/profile" element={<ProfilePage />} />
-
                 <Route path="/application/:applicationId/review" element={<ApplicationReviewPage />} />
                 <Route path="/application/:applicationId/assessment/rules" element={<AssessmentRulesPage />} />
                 <Route path="/application/:applicationId/assessment/start" element={<AssessmentPage />} />
-
-
                 <Route path="/application/:applicationId/assessment" element={<AssessmentPage />} />
                 <Route path="/application/:applicationId/result" element={<ApplicationResultPage />} />
-
-                
-                
-
             </Route>
 
             <Route path="*" element={<Navigate to="/login" replace />} />

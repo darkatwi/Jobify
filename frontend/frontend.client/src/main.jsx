df760c2632393dba0101d819d8e806122d76dfe3
Rochan Sabek <rms134@mail.aub.edu>
2026-03-21 15:52:33 +0200

    wrap entire frontend application with theme provider

--- frontend/frontend.client/src/main.jsx
+++ frontend/frontend.client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { ThemeProvider } from "./layout/ThemeProvider.jsx";
-
+// wrap it around the whole app
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(

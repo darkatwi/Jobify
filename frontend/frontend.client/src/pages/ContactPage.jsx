49b5f05a65cb75ea903e1d92722ddeb9bedbf813
Rochan Sabek <rms134@mail.aub.edu>
2026-03-28 19:13:49 +0200

    create contactpage for footer navigation

---
+++ frontend/frontend.client/src/pages/ContactPage.jsx
@@ -0,0 +1,9 @@
+export default function ContactPage() {
+  return (
+    <div style={{ padding: "24px" }}>
+      <h1>Contact</h1>
+      <p>For questions about Jobify, please contact the project team.</p>
+      <p>Contact information will be added soon.</p>
+    </div>
+  );
+}

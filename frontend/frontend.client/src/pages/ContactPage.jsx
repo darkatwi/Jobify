113282c2e88bec3925812d2d66bc22236366f40d
Rochan Sabek <rms134@mail.aub.edu>
2026-03-28 19:41:12 +0200

    remove previous footer separate pager after inner implementation

--- frontend/frontend.client/src/pages/ContactPage.jsx
+++
@@ -1,9 +0,0 @@
-export default function ContactPage() {
-  return (
-    <div style={{ padding: "24px" }}>
-      <h1>Contact</h1>
-      <p>For questions about Jobify, please contact the project team.</p>
-      <p>Contact information will be added soon.</p>
-    </div>
-  );
-}
